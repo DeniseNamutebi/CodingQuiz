a878e9d9d6cadaa767bd77f20fe7701d1cb778b1
Ayisha Alli <alli.ayisha1@gmail.com>
2021-03-25 20:49:44 +0000

    hightlighted text with correct/wrong answer

--- app.js
+++ app.js
@@ -1,41 +1,75 @@
 //Variables
-// 'use strict';
+'use strict';
 
 const data = [
-    {"id": 1,
+  {
+    "id": 1,
     "Question": "What does HTML stand for?",
     "options": {
-    "a": "HyperTextMarkup Language",
-    "b": "HelloText Markup Langauge",
-    "c": "HyperText Money Language"}
-    }
+      "a": "HyperTextMarkup Language",
+      "b": "HelloText Markup Langauge",
+      "c": "HyperText Money Language",
+    },
+    "answer": "a"
+  }
 ];
-
-const qnNum = document.getElementById('qn-number');
-const qn = document.getElementById('qn');
-
+// Variables = Accessing DOM
+const qnNum = document.getElementById("qn-number");
+const qn = document.getElementById("qn");
 
 qnNum.innerHTML = data[0]["id"];
-qn.innerHTML = data[0]["Question"]
-const optionInput = data[0]["options"]
-
-
+qn.innerHTML = data[0]["Question"];
+const optionInput = data[0]["options"];
 
 let optionArray = "";
 
-for(const [key, value] of Object.entries(optionInput)){
-     optionArray += `<input type="radio" name="${key}" value="${key}" id="${key}" onclick="selectOnlyThis(this.id)" > ${value} </input>`
- }
-
+// Creates HTML elements which are the options to the question 
+for (const [key, value] of Object.entries(optionInput)) {
+  optionArray += `<div class="option" onclick="correctAnswer(this.id)" id="${key}"><input type="radio" name="option" class="option" value="${key}" > ${value}</input></div> `;
+  
+}
 document.querySelector("#quizContainer").innerHTML = optionArray;
 
+document.addEventListener("click", function (e) {
+    //console.log(e.target)
+})
 
-function selectOnlyThis(id){
-    for (let i = 1; i < optionInput.length; i++){
-        document.getElementById(i).checked = false;
+
+
+// Selecting the correct/wrong answer and adding colour
+function correctAnswer (id){
+     /* document.querySelector(".option").click(function(event){
+         event.preventDefault()
+         console.log(document.querySelector(".option"));  
+         
+        } 
+    ) */
+    if (id === data[0]["answer"]){
+        document.getElementById(id).style.color = "green"
+        //console.log(document.getElementById(id))
+        
     }
-   document.getElementById(id).checked = true;
+    else{ 
+        document.getElementById(id).style.color = "red"
+        }  
 }
+
+
+/* function selectOnlyThis(id) {
+  for (let i = 1; i < optionInput.length; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+} */
+
+/* function functionCaller (id){
+    //selectOnlyThis(id)
+ correctAnswer()
+} */
+
+
+
+
 
 
 
